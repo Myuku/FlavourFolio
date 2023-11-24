e95b62389f7780267d0acb987fc7e35edc99c348
Myuku <junelee2002jh@gmail.com>
2023-11-23 22:09:20 -0800

    Testing for main branch

--- app/src/main/java/com/example/flavourfolio/tabs/steps/StepsFragment.kt
+++ app/src/main/java/com/example/flavourfolio/tabs/steps/StepsFragment.kt
@@ -6,27 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import com.example.flavourfolio.R
 
 class StepsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = StepsFragment()
-    }
-
     private lateinit var viewModel: StepsViewModel
+    private lateinit var tvCounter: TextView
+    private lateinit var button: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        viewModel = ViewModelProvider(this)[StepsViewModel::class.java]
         return inflater.inflate(R.layout.fragment_steps, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(StepsViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvCounter = view.findViewById(R.id.counter)
+        tvCounter.text = viewModel.counter.toString()
 
+        button = view.findViewById(R.id.button)
+        button.setOnClickListener {
+            viewModel.incrementCounter()
+            tvCounter.text = viewModel.counter.toString()
+        }
+    }
 }
--- app/src/main/java/com/example/flavourfolio/tabs/steps/StepsViewModel.kt
+++ app/src/main/java/com/example/flavourfolio/tabs/steps/StepsViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.flavourfolio.tabs.steps
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StepsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var counter = 0
+        private set
+
+    fun incrementCounter() {
+        counter++
+    }
 }
