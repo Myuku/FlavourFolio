44bf40471b69431b844cfdc6bbbdb178ab4d3ea4
Myuku <junelee2002jh@gmail.com>
2023-11-24 02:21:38 -0800

    Finished implementing Step-by-Step base UI

--- app/src/main/java/com/example/flavourfolio/tabs/steps/StepsViewModel.kt
+++ app/src/main/java/com/example/flavourfolio/tabs/steps/StepsViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.flavourfolio.tabs.steps
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class StepsViewModel : ViewModel() {
-    var counter = 0
+    var currProgress = 1
+        private set
+    var maxSteps = 10 // TODO: Always do numSteps + 1 to display complete screen
         private set
 
-    fun incrementCounter() {
-        counter++
+    fun incrementStep(): Int {
+        if (currProgress == maxSteps) { return 1 }
+        currProgress++
+        return 0
+    }
+    fun decrementStep(): Int {
+        if (currProgress == 1) { return 1 }
+        currProgress--
+        return 0
     }
 }
