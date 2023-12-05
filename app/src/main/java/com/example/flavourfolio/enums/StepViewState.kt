26105cece8e6255385a1d5932f178b6985dbe4b4
JLJinhanLiu <jinhan_liu@sfu.ca>
2023-12-05 04:44:49 -0800

    Fix wrong viewstate id for PICTURE and TIMER

--- app/src/main/java/com/example/flavourfolio/enums/StepViewState.kt
+++ app/src/main/java/com/example/flavourfolio/enums/StepViewState.kt
@@ -2,8 +2,8 @@ package com.example.flavourfolio.enums
 
 // Enum to make it easier to know what state is being used
 enum class StepViewState(val idx: Int) {
-    PICTURE(0),
-    TIMER(1),
+    TIMER(0),
+    PICTURE(1),
     START(2),
     DONE(3),
 }
